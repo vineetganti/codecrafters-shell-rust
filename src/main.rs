6b4f8ca268ea0dead11d930c287daafe96297af8
vineet <vineetganti@gmail.com>
2026-09-10 21:21:04 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,11 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // Handling invalid commands
+    // Captures the user's command in the command variable
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    //Prints the "<command> : command not found" message to the console
+    println!("{}: command not found", command.trim());
 }
