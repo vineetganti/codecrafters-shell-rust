a6c37e322b130adc72b9acdb30eb5d1f53dd55e5
vineet <vineetganti@gmail.com>
2026-09-10 22:59:11 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Handling invalid commands
-    // Captures the user's command in the command variable
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    //Prints the "<command> : command not found" message to the console
-    println!("{}: command not found", command.trim());
+    loop {
+        // TODO: Uncomment the code below to pass the first stage
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        
+        // Handling invalid commands
+        //Captures the user's command in the command variable
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        //Prints the "<command> : command not found" message to the console
+        println!("{}: command not found", command.trim());
+    }
 }
